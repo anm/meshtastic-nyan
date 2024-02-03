8b97d900ece551808712b483ff6b90e20696853c
River MacLeod <river@mm0hai.net>
2024-02-03 18:25:53 +0000

    tbeam needs slow programming speed.

--- src/modules/NyanModule.h
+++ src/modules/NyanModule.h
@@ -8,14 +8,14 @@ class NyanModule : public ProtobufModule<nyan_telemetry>,
                    private concurrency::OSThread {
   public:
   /* Constructor name is for debugging output */
-  NyanModule() :
-    ProtobufModule("nyan", meshtastic_PortNum_NYAN, &nyan_telemetry_msg),
-    concurrency::OSThread("NyanModule") {}
+  NyanModule();
 
   protected:
 
   virtual int32_t runOnce() override;
   virtual bool handleReceivedProtobuf(const meshtastic_MeshPacket &mp, nyan_telemetry *telemetry) override;
 
+  void report_compilation_task(void *params);
+  static void sensor_sampler_task(void *params);
   void send_report();
 };
