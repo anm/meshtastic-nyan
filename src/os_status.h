33101e39f00c10dee895906c825a9bfbacd576d8
River MacLeod <git@mm0hai.net>
2025-02-08 01:37:43 +0000

    Removed FreeRTOS polyfill

--- src/os_status.h
+++ src/os_status.h
@@ -2,7 +2,9 @@
 
 /* FreeRTOS monitoring / introspection. */
 
-void vTaskGetRunTimeStats(signed char *pcWriteBuffer);
+// This seems to have been added to the Arduino build of FreeRTOS so don't
+// need to add it anymore.
+//void vTaskGetRunTimeStats(signed char *pcWriteBuffer);
 
 /* Return number of tasks. */
 unsigned int task_count();
